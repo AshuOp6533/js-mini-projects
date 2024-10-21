0b18447ed1feb0a99859ec185975fe82b7034587
Ashutosh Sinha (aptlogica) <ashutosh.sinha@aptl.local>
2024-10-21 15:04:53 +0530

    signup signin added

---
+++ signup signin/index.js
@@ -0,0 +1,9 @@
+function showSignIn() {
+    document.getElementById("container1").style.display = "none";
+    document.getElementById("container2").style.display = "block";
+  }
+
+  function showSignUp() {
+    document.getElementById("container2").style.display = "none";
+    document.getElementById("container1").style.display = "block";
+  }
