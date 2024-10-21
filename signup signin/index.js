76a438fee008ba46bbe0c4874ea384d205a55c55
Ashutosh Sinha (aptlogica) <ashutosh.sinha@aptl.local>
2024-10-21 17:27:22 +0530

    signup signin done

--- signup signin/index.js
+++ signup signin/index.js
@@ -1,9 +1,53 @@
-function showSignIn() {
-    document.getElementById("container1").style.display = "none";
-    document.getElementById("container2").style.display = "block";
-  }
+let data = {};
 
-  function showSignUp() {
-    document.getElementById("container2").style.display = "none";
-    document.getElementById("container1").style.display = "block";
-  }
+function showSignIn() {
+  document.getElementById("container1").style.display = "none";
+  document.getElementById("container2").style.display = "block";
+}
+function showSignUp() {
+  document.getElementById("container2").style.display = "none";
+  document.getElementById("container1").style.display = "block";
+}
+
+let signup = document
+  .getElementById("signupbutton")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm").value;
+
+    if (password !== confirmPassword) {
+      alert("password do not match");
+      return;
+    }
+    data[username] = {
+      email: email,
+      password: password
+    };
+    alert('signup successful')
+    console.log("sign up successfull", data);
+    
+    showSignIn();
+  });
+  let signin = document.getElementById("signinbutton").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let username = document.getElementById("user").value;
+    let password = document.getElementById("pass").value;
+
+    if(data[username]){
+        if(data[username].password === password){
+            alert("Sign In successfull")
+            console.log(data[username]);            
+        }
+        else{
+            alert("incorrect password")
+        }
+    }
+    else{
+        alert("user not found")
+    }
+  })
